ba163a78ba8d98caf2cee75bedcb70457034d6ee
Austin Jackson <themacphage@gmail.com>
2014-08-08 23:36:21 -0500

    Added IOTool.java again

---
+++ src/com/ruthlessphysics/util/IOTool.java
@@ -0,0 +1,25 @@
+package com.ruthlessphysics.util;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class IOTool
+{
+	/*
+	 * From:
+	 * http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from-the-contents-of-a-file
+	 * 
+	 */
+	public static String readFile(String path, Charset encoding) throws IOException 
+	{
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
+	public static String readFile(String path) throws IOException 
+	{
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, Charset.defaultCharset());
+	}
+}
